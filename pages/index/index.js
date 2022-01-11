78481b9641559bbb92f6202853a9843cd79b2c03
LuoJie <1299124551@qq.com>
2022-01-11 09:34:33 +0800

    （12章) 复习vue数据带来劫持

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,48 +1,22 @@
-// index.js
-// 获取应用实例
-const app = getApp()
-
+/*
+ * @Descripttion: 
+ * @Company: 思宏科技
+ * @version: v1.0
+ * @Author: LuoJie
+ * @Date: 2022-01-11 09:07:50
+ * @LastEditors: LuoJie
+ * @LastEditTime: 2022-01-11 09:17:18
+ */
 Page({
-  data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo'),
-    canIUseGetUserProfile: false,
-    canIUseOpenData: wx.canIUse('open-data.type.userAvatarUrl') && wx.canIUse('open-data.type.userNickName') // 如需尝试获取用户信息可改为false
+  // 页面的初始化数据
+  data:{
+    msg: '咕咕唧唧'
   },
-  // 事件处理函数
-  bindViewTap() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
-  onLoad() {
-    if (wx.getUserProfile) {
-      this.setData({
-        canIUseGetUserProfile: true
-      })
-    }
-  },
-  getUserProfile(e) {
-    // 推荐使用wx.getUserProfile获取用户信息，开发者每次通过该接口获取用户个人信息均需用户确认，开发者妥善保管用户快速填写的头像昵称，避免重复弹窗
-    wx.getUserProfile({
-      desc: '展示用户信息', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
-      success: (res) => {
-        console.log(res)
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    })
-  },
-  getUserInfo(e) {
-    // 不推荐使用getUserInfo获取用户信息，预计自2021年4月13日起，getUserInfo将不再弹出弹窗，并直接返回匿名的用户个人信息
-    console.log(e)
+  onLoad: function (options) {
+    console.log(this) //this代表当前页面的实例对象
     this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
+      msg :'修改后的咕咕唧唧'
     })
+    console.log(this.data.msg) //因为底层没有实现数据代理，所以需要this.data才能取到data中的数据
   }
 })
